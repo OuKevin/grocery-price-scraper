a5cdfffb0052c7ac47534f32a0f2d22daf29bf32
Kevin Ou <therealkevinou@yahoo.com>
2020-03-02 21:07:05 -0600

    Update constants.js
    included constant for red grapes

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const STRAWBERRY = 500614277;
 export const BANANA = 500613767;
+export const RED_GRAPES = 538987997;
 
-export const itemsToScrape = [STRAWBERRY, BANANA];
+export const itemsToScrape = [BANANA];
