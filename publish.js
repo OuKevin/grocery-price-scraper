384de91eb738c205f3143f54145a1ad0837811d9
KKOU <kevin.ou@bayer.com>
2020-03-06 21:17:44 -0600

    added zip and publish script

---
+++ publish.js
@@ -0,0 +1,27 @@
+/* eslint-disable global-require */
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
+
+const { AWS_REGION } = process.env;
+
+(async () => {
+  const lambda = new AWS.Lambda({
+    apiVersion: '2015-03-31',
+    region: AWS_REGION,
+  });
+
+  const lambdaName = 'grocery-price-scraper';
+  const zipPath = `${path.resolve()}/dist/archive.zip`;
+
+  try {
+    await lambda.updateFunctionCode({
+      FunctionName: lambdaName,
+      ZipFile: fs.readFileSync(zipPath),
+    }).promise();
+  } catch (error) {
+    console.error(`Could not save ${lambdaName}, ${JSON.stringify(error)}`);
+  }
+})();
